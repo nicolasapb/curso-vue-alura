1a66bc9a74b43688dd29bbae4f3157ab158f568b
Nicolas <nicolas.a.p.barbosa@gmail.com>
2018-09-15 08:28:16 -0300

    vue 1.2.4

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>{{ titulo }}</h1>
-    <img :src="foto.url" :alt="foto.titulo">
+    <ul>
+      <li v-for="foto of fotos" v-bind:key="foto.id">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -10,10 +14,18 @@ export default {
   data() {
     return {
       titulo: 'Alurapic',
-      foto: {
-        url: 'https://picsum.photos/g/450/450/?image=76',
-        titulo: 'bicicleta'
-      }
+      fotos: [
+        {
+          id: '1',
+          url: 'https://picsum.photos/g/450/450/?image=76',
+          titulo: 'bicicleta'
+        },
+        {
+          id: '2',
+          url: 'https://picsum.photos/g/450/450/?image=83',
+          titulo: 'bosque'
+        }
+      ]
     }
   }
 }
