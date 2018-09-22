a09d693bcc71cba17d2832b16c3d588b449f901e
Nicolas <nicolas.a.p.barbosa@gmail.com>
2018-09-22 10:38:45 -0300

    vue 1.6.7

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="corpo">
+    <nav>
+      <ul>
+          <li v-for="(route, i) in routes" :key="i">
+            <router-link :to="route.path ? route.path : '/'">{{ route.titulo }}</router-link>
+          </li>
+
+      </ul>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+
+import { routes } from './routes';
+
 export default {
 
+  data() {
+    return {
+      routes
+    }
+  }
 }
 </script>
 
